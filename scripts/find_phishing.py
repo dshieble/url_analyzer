983da0f0687ea8fb44bfde11ea0b7836137d6579
Dan Shiebler <danshiebler@gmail.com>
2024-09-13 06:37:14 -0400

    fix

--- scripts/find_phishing.py
+++ scripts/find_phishing.py
@@ -9,13 +9,14 @@ import certifi
 import certstream
 from termcolor import colored
 
-sys.path.append(os.path.join(os.path.join(os.path.dirname(__file__), ".."), ".."))
 from url_analyzer.phishing_stream.processor import Processor
 
 CERTSTREAM_URL = 'wss://certstream.calidog.io'
 
 if __name__ == '__main__':
-  # python url_analyzer/phishing_stream/find_phishing.py
+  """
+  python scripts/find_phishing.py
+  """
   sslopt = {"cert_reqs": ssl.CERT_REQUIRED, "ca_certs": certifi.where()}
   processor = Processor()
   certstream.listen_for_events(processor.callback, url=CERTSTREAM_URL, sslopt=sslopt)
