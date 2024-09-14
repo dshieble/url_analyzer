7fc1c94a177f70274cacfa4510b3d686605bc503
Dan Shiebler <danshiebler@gmail.com>
2024-09-14 11:16:07 -0400

    fix

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -11,13 +11,26 @@ from url_analyzer.classification.classification import BasicUrlClassifier
 
 class TestEndToEnd(unittest.TestCase):
 
-  def test_end_to_end(self):
+  def test_end_to_end_1(self):
     maybe_rich_classification_response = asyncio.run(BasicUrlClassifier().classify_url(
       url="https://danshiebler.com"
     ))
     assert maybe_rich_classification_response.content.url_classification.is_phishing == False
     assert maybe_rich_classification_response.content.url_classification.page_state == "Active"
 
+
+  def test_end_to_end_2(self):
+    maybe_rich_classification_response = asyncio.run(BasicUrlClassifier().classify_url(
+      url="https://danshiebler.com/fake"
+    ))
+    assert maybe_rich_classification_response.content.url_classification.is_phishing == False
+    assert maybe_rich_classification_response.content.url_classification.page_state == "404"
+
+  def test_end_to_end_3(self):
+    maybe_rich_classification_response = asyncio.run(BasicUrlClassifier().classify_url(
+      url="fake"
+    ))
+    assert maybe_rich_classification_response.error is not None
 
 
 if __name__ == '__main__':
